2a85c319fd38c17bb6cf79c4362c88207da673fc
zeritos <31357981+zeritos@users.noreply.github.com>
2017-08-29 01:01:31 +0100

    Test Api v1

---
+++ testing-api/src/main/java/com/example/testingapi/TestingApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.testingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApiApplication.class, args);
+	}
+}
---
+++ testing-api/src/main/java/com/example/testingapi/topic/TopicService.java
@@ -0,0 +1,26 @@
+package com.example.testingapi.topic;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core java", "Core Java description"),
+			new Topic("js", "JavaScritp", "JS Description")
+			);
+	
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+}
+
+
